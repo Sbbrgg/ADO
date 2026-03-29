694b98d83b4dc4c116d28f1094d97071d8888205
un <un@DESKTOP-JAU235M>
2026-03-29 20:38:18 +0300

    dataGridVies in progress

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -13,14 +13,40 @@ namespace Academy
 {
 	public partial class MainForm : Form
 	{
+		Query[] queries =
+		{
+			new Query
+				(
+				"last_name,first_name,middle_name,group_name,direction_name",
+				"Students,Groups,Directions",
+				"[group]=group_id AND direction=direction_id"
+				),
+			new Query
+				(
+				"*",
+				"Groups,Directions",
+				"direction=direction_id"
+				),
+			new Query("*", "Directions"),
+			new Query("*", "Disciplines"),
+			new Query("*", "Teachers")
+		};
+		DataGridView[] tables;
 		DBTools.Connector connector;
 		public MainForm()
 		{
 			InitializeComponent();
+			tables = new DataGridView[] { dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers };
 			connector = new DBTools.Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
-			dgvDirections.DataSource = connector.Select("*", "Directions");
-			toolStripStatusLabel.Text = $"Количество направлений обучения: {dgvDirections.Rows.Count-1}";
+
+			//dgvDirections.DataSource = connector.Select("*", "Directions");
+			//toolStripStatusLabel.Text = $"Количество направлений обучения: {dgvDirections.Rows.Count-1}";
 			//toolStripStatusLabel.Text = $"Количество направлений обучения: {connector.Scalar("SELECT COUNT(*) FROM Directions")}";
+		}
+
+		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
