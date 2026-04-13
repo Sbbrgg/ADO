6212942274d84dc509226e1b23d721d10331836d
un <un@DESKTOP-JAU235M>
2026-04-13 20:18:57 +0300

    Add new Student with photo

--- Academy/StudentForm.cs
+++ Academy/StudentForm.cs
@@ -48,11 +48,21 @@ namespace Academy
 
 			student = new Models.Student(human, Convert.ToInt32(cbGroup.SelectedValue));
 			//object id = DataBase.Connector.Scalar($"SELECT stud_id FROM Students WHERE {student.GetCondition()}");
-			if (student.id == 0) DataBase.Connector.Insert("Students", $"{student.GetNames()}", $"{student.GetValues()}");
+			if (student.id == 0)student.id =  
+					Convert.ToInt32(DataBase.Connector.Scalar
+					(
+						$"INSERT Students({student.GetNames()}) VALUES ({student.GetValues()}); SELECT SCOPE_IDENTITY()"
+					));
 			else DataBase.Connector.Update($"UPDATE Students SET {student.GetUpdateString()} WHERE stud_id={student.id}");
 			if (student.photo != null)
+			{
+				//if (student.id == 0)
+				//	student.id = (int)DataBase.Connector.Scalar
+				//		(
+				//		$"SELECT stud_id FROM Students WHERE {student.GetCondition()}"
+				//		);
 				DataBase.Connector.UploadPhoto(student.SerializePhoto(), student.id, "photo", "Students");
-
+			}
 			//DataBase.Connector.Insert
 			//	(
 			//	"Students",
