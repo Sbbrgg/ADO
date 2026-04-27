4d7f0b097bd55b348f1d9124a9c9e1939657c8ff
un <un@DESKTOP-JAU235M>
2026-04-27 16:52:40 +0300

    filters in progress

--- AcademyWPF/MainWindow.xaml.cs
+++ AcademyWPF/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Configuration;
 using DBTools;
 using Microsoft.Win32;
+using System.Data;
 
 namespace AcademyWPF
 {
@@ -24,14 +25,53 @@ namespace AcademyWPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Query[] queries =
+		{
+			new Query
+				(
+				"stud_id,last_name,first_name,middle_name,group_name,direction_name",
+				"Students,Groups,Directions",
+				"[group]=group_id AND direction=direction_id"
+				),
+			new Query
+				(
+				"*",
+				"Groups,Directions",
+				"direction=direction_id"
+				),
+			new Query("*", "Directions"),
+			new Query("*", "Disciplines"),
+			new Query("*", "Teachers"),
+		};
+		string[] status_messages =
+		{
+			"Количество студентов",
+			"Количество групп",
+			"Количество направлений",
+			"Количество дисциплин",
+			"Количество преподавателей"
+		};
 		Connector connector;
 		DataGrid[] tables;
+		private Dictionary<string, int> d_directions;
+		private Dictionary<string, int> d_groups;
 		public MainWindow()
 		{
 			InitializeComponent();
 			connector = new Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
 			tables = new DataGrid[] { dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers };
-			
+
+			d_groups = connector.GetDictionary("Groups");
+			d_directions = connector.GetDictionary("Directions");
+
+			List<string> l_direction = new List<string> { "" };
+			l_direction.AddRange(d_directions.Keys);
+			cbDirections_dgvStudents.ItemsSource = l_direction;
+
+			List<string> l_groups = new List<string> { "" };
+			l_groups.AddRange(d_groups.Keys);
+			cbGroups_dgvStudents.ItemsSource = l_groups;
+
 			tabCotrol.SelectedIndex = 0;
 		}
 
@@ -42,5 +82,23 @@ namespace AcademyWPF
 				Select($"SELECT * FROM {((sender as TabControl).Items[i] as TabItem).Header.ToString()}").DefaultView;
 			statusBarCount.Text = $"Количество записей: {tables[i].Items.Count - 1}";
 		}
-	}
+		private void cbDirections_dgvStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			
+        }
+		private void cbGroups_dgvStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			//dgvGroups.ItemsSource = connector.Select
+			//	(
+			//		queries[1].ToString() +
+			//		$" AND Direction={d_directions[cbDirections_dgvStudents.SelectedItem.ToString()]}"
+			//	);
+        }
+		private void btnClearFilters_dgvStudents_Click(object sender, RoutedEventArgs e)
+		{
+			cbGroups_dgvStudents.SelectedItem= null;
+			cbDirections_dgvStudents.SelectedItem = null;
+        }
+
+    }
 }
